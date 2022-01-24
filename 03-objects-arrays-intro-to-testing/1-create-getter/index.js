a47074d30962b824d1b55333aee5cdd2f35b60e1
Mykhailo <myk@Mykhailos-iMac.lan>
2022-01-24 12:09:48 +0000

    trim solved

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,7 +4,8 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  const pathSplit = path.split('.');
   return function (obj) {
-    return path.split('.').reduce((previousValue, item) => previousValue?.[item], obj);
+    return pathSplit.reduce((previousValue, item) => previousValue?.[item], obj);
   };
 }
