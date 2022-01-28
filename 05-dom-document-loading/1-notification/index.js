2c8568b40a983f6760abe3923e11a9eaae54e1d0
Mykhailo <myk@Mykhailos-iMac.lan>
2022-01-28 11:58:13 +0000

    notification solved

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,82 @@
 export default class NotificationMessage {
+  element;
 
+  notificationTypes = {
+    success: 'success',
+    error: 'error'
+  };
+
+  static activeNotification = null;
+
+  constructor(message = '', {
+    duration = 0,
+    type = '',
+  } = {}) {
+
+    this.message = message;
+    this.duration = duration;
+    this.type = type;
+
+    this.render();
+  }
+
+  getTemplate() {
+    return `
+      <div class="notification ${this.notificationTypes[this.type]}" style="--value:${this.duration + 's'}">
+        <div class="timer"></div>
+            <div class="inner-wrapper">
+                <div class="notification-header">${this.type}</div>
+                <div class="notification-body">
+                ${this.message}
+            </div>
+        </div>
+      </div>
+    `;
+  }
+
+  render() {
+    const element = document.createElement('div');
+    element.innerHTML = this.getTemplate();
+    this.element = element.firstElementChild;
+  }
+
+  cleanActiveNotification() {
+    if (NotificationMessage.activeNotification) {
+      NotificationMessage.activeNotification.remove();
+      NotificationMessage.activeNotification = null;
+    }
+  }
+
+  show(target) {
+    this.cleanActiveNotification();
+
+    NotificationMessage.activeNotification = this.element;
+
+    if (target) {
+      target.append(this.element);
+    }
+    else {
+      document.body.append(this.element);
+    }
+
+    setTimeout(() => {
+      this.remove();
+    }, this.duration);
+
+  }
+
+  destroy() {
+    if (this.element) {
+      this.element.remove();
+      this.element = null;
+      NotificationMessage.activeNotification = null;
+    }
+  }
+
+  remove() {
+    if (this.element) {
+      this.element.remove();
+    }
+  }
 }
+
